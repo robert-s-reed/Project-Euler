babda7966166bdf326c631401ccbc82029f84765
Robert Reed <rob.s.reed@gmail.com>
2020-06-08 20:24:00 +0100

    Updates formatting

--- Project Euler/p001.py
+++ Project Euler/p001.py
@@ -9,4 +9,4 @@ for multipleOf3 in range(3, 1000, 3):
 for multipleOf5 in range(5, 1000, 5):
     if multipleOf5 % 3 != 0:
         sum += multipleOf5
-print("Answer = " + str(sum) + " in " + "{:.3f}".format(time.time() - startTime))
+print("Answer = " + str(sum) + " in {:.3f}".format(time.time() - startTime))
