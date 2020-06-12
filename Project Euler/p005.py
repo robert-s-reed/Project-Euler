52a611c998ad08ab2bb8b87bb3640ad9159a167c
Robert Reed <rob.s.reed@gmail.com>
2020-06-12 15:57:59 +0100

    Added p005

---
+++ Project Euler/p005.py
@@ -0,0 +1,20 @@
+# Problem 5: Smallest multiple
+
+import time
+
+startTime = time.time()
+maxVal = 20
+interval = maxVal * (maxVal - 1)
+
+x = interval
+while True:
+    isAnswer = 1
+    for y in range(maxVal, 0, -1):
+        if x % y != 0:
+            isAnswer = 0
+            break
+    if isAnswer == 1:
+        print("Answer = " + str(x) + " in {:.3f}".format(time.time() - startTime) + "s")
+        break
+    else:
+        x += interval
